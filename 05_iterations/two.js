c491e350ecdfcb7dffe6e1a643451655a8d6397a
Daksharma19 <shyper73@gmail.com>
2024-12-24 19:35:24 +0530

    do while and while done

---
+++ 05_iterations/two.js
@@ -0,0 +1,13 @@
+
+let myArray = ["flash", "superman", "batman", "wonderwoman"]
+let index = 0;
+while(index < myArray.length) {
+    console.log(myArray[index])
+    index++
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`)
+    score++
+}while(score <= 10)
